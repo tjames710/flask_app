cb2229d78b110e014c15527a2d9fad37e7e0a748
DESKTOP-7E9AP26\t_jam <trunyonworks@gmail.com>
2017-07-17 20:13:39 -0400

    added the templates directory and started using render_template!

--- simple_app.py
+++ simple_app.py
@@ -1,11 +1,12 @@
-from flask import Flask 
+from flask import Flask
+from flask import render_template 
 
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/<name>')
 def index(name='Ted'):
-	return "Hello it's {}!".format(name)
+	return render_template("index.html", name=name)
 
 
 @app.route('/add/<float:num1>/<float:num2>')
@@ -13,7 +14,8 @@ def index(name='Ted'):
 @app.route('/add/<int:num1>/<float:num2>')
 @app.route('/add/<float:num1>/<int:num2>')
 def add(num1, num2):
-	return '{} + {} = {}'.format(num1, num2, num1 + num2)
+	return render_template("add.html", num1=num1, num2=num2)
+
 	
 
 
