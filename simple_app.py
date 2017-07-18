57fec2091b414eba463b745e0009018f21330913
DESKTOP-7E9AP26\t_jam <trunyonworks@gmail.com>
2017-07-17 20:00:10 -0400

    learned more about routes and views. converting urls to ints or floats

--- simple_app.py
+++ simple_app.py
@@ -1,12 +1,20 @@
 from flask import Flask 
-from flask import request
 
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/<name>')
 def index(name='Ted'):
-	name = request.args.get('name', name)
 	return "Hello it's {}!".format(name)
+
+
+@app.route('/add/<float:num1>/<float:num2>')
+@app.route('/add/<int:num1>/<int:num2>')
+@app.route('/add/<int:num1>/<float:num2>')
+@app.route('/add/<float:num1>/<int:num2>')
+def add(num1, num2):
+	return '{} + {} = {}'.format(num1, num2, num1 + num2)
+	
 
 
 app.run(debug=True) 
