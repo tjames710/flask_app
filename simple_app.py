0c5c1ed34d667be1e444550492293203157a214b
DESKTOP-7E9AP26\t_jam <trunyonworks@gmail.com>
2017-07-19 19:56:53 -0400

    added a redirect for the back button

--- simple_app.py
+++ simple_app.py
@@ -1,21 +1,17 @@
-from flask import Flask, render_template, request, redirect, url_for
+import json
+from flask import (Flask, render_template, request, redirect,
+				  url_for, make_response)
 
 
 app = Flask(__name__)
 
 @app.route('/')
-@app.route('/index', methods=['GET'])
 def index():
 	return render_template("index.html")
 
-
-@app.route('/add/<float:num1>/<float:num2>')
-@app.route('/add/<int:num1>/<int:num2>')
-@app.route('/add/<int:num1>/<float:num2>')
-@app.route('/add/<float:num1>/<int:num2>')
-def add(num1, num2):
-	return render_template("add.html", num1=num1, num2=num2)
-
+@app.route('/index', methods=['GET'])
+def back():
+	return redirect(url_for('index'))
 
 @app.route('/save', methods=['POST'])
 def save():
@@ -29,3 +25,11 @@ def save():
 
 
 app.run(debug=True) 
+
+
+# @app.route('/add/<float:num1>/<float:num2>')
+# @app.route('/add/<int:num1>/<int:num2>')
+# @app.route('/add/<int:num1>/<float:num2>')
+# @app.route('/add/<float:num1>/<int:num2>')
+# def add(num1, num2):
+# 	return render_template("add.html", num1=num1, num2=num2)
